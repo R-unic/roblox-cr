550c444032dd3a2ae968503af3909201a5e4c9f5
R-unic <roleyspersonal@gmail.com>
2023-05-06 02:03:15 -0700

    build: add placeholder spec

---
+++ spec/main_spec.cr
@@ -0,0 +1,10 @@
+require "./spec_helper"
+
+describe CodeGenerator do
+  # TODO: Write tests
+  describe "#generate" do
+    it "should check types before calling" do
+      true.should eq(true)
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/roblox-cr"
+require "../src/code_generator"
