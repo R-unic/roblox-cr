e3ad7a4e239fb730d7726c51a6f0d4c0c457a1cb
R-unic <roleyspersonal@gmail.com>
2023-05-09 15:50:42 -0700

    test: refactor tests

--- spec/main_spec.cr
+++ spec/main_spec.cr
@@ -1,80 +1,69 @@
 require "./spec_helper"
 
-def get_lua(source : String) : String
-  CodeGenerator.new(source, GenerationMode::Module, testing: true).generate
+def get_lua_lines(source : String) : Array(String)
+  result = CodeGenerator.new(source, GenerationMode::Module, testing: true)
+    .generate
+    .split('\n')
+    .map(&.strip)
+    .select { |line| line != "" }
+
+  result.shift
+  result.shift
+  result
 end
 
 describe CodeGenerator do
   describe "#generate" do
     it "should generate hello world" do
-      lua = get_lua %q{
+      lines = get_lua_lines %q{
         puts "hello world"
       }
-      lua.split('\n').last.should eq "print(\"hello world\")"
+      lines.shift.should eq "print(\"hello world\")"
     end
     it "should check if instance member is method before calling" do
-      lua = get_lua %q{
+      lines = get_lua_lines %q{
         class A
           @member : String = "yup"
         end
         A.new.member
       }
-      lua.split('\n')[-2].should eq "local _ = (type(A.new().Member) == \"function\" and A.new():Member() or A.new().Member);"
+      lines.last.should eq "local _ = (type(A.new().Member) == \"function\" and A.new():Member() or A.new().Member);"
     end
     it "should check if class member is method before calling" do
-      lua = get_lua %q{
+      lines = get_lua_lines %q{
         class A
           @@member : String = "yup"
         end
         A.member
       }
-      lua.split('\n')[-2].should eq "local _ = (type(A.Member) == \"function\" and A.Member() or A.Member);"
+      lines.last.should eq "local _ = (type(A.Member) == \"function\" and A.Member() or A.Member);"
     end
     it "should check if referenced variable is method before calling" do
-      lua = get_lua %q{
+      lines = get_lua_lines %q{
         def something
           puts "a"
         end
         something
       }
-      lua.split('\n')[-2].should eq "local _ = (type(Something) == \"function\" and Something() or Something);"
+
+      lines.shift.should eq "function Something()"
+      lines.shift.should eq "return print(\"a\")"
+      lines.shift.should eq "end"
+      lines.shift.should eq "local _ = (type(Something) == \"function\" and Something() or Something);"
     end
     it "should support basic single inheritance" do
-      lua = get_lua %q{
-        class Animal
-          getter type : String
-
-          def initialize(@type)
-          end
-
-          def dog?
-            type == "Dog"
-          end
+      lines = get_lua_lines %q{
+        def fib(n : Int) : Int
+          n <= 1 ? n : (fib(n - 1) + fib(n - 2))
         end
 
-        class Dog < Animal
-          getter name : String
-          getter breed : String
-
-          def initialize(@name, @breed)
-            super "Dog"
-          end
-
-          def bark
-            puts "woof"
-          end
-        end
-
-        dog = Dog.new "Bentley", "Border Collie"
-        puts dog.is_a?(Dog)
-        puts dog.is_a?(Animal)
-        puts dog.name, dog.type
-        dog.bark
-        puts dog.dog?
+        puts fib 10 #=> 55
       }
 
-      lua_lines = lua.split '\n'
-      # TODO: check lua output
+      lines.shift.should eq "function Fib(N)"
+      lines.shift.should eq "return (N <= 1 and N or Fib(N - 1) + Fib(N - 2))"
+      lines.shift.should eq "end"
+      lines.shift.should eq "print(Fib(10))"
     end
   end
 end
